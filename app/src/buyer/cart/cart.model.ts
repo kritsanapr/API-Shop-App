9f82c8565463c7d74d0ecb21aeb632c88f30cf82
kritsanapr <kritsana.pr.41@gmail.com>
2023-06-23 16:20:01 +0700

    feat : create cart model

---
+++ app/src/buyer/cart/cart.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+import { CartDoc, CartModel } from '@shoppingapp/common';
+
+const schema: Schema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CartProduct',
+        required: true
+    }],
+    totalPrice: {
+        type: Number,
+        required: true
+    }
+})
+
+export const Cart = mongoose.model<CartDoc, CartModel>('Cart', schema);
